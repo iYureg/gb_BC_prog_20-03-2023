9f213a7c7c935149e03deff29887c4555db54726
Yurii <iyureg@gmail.com>
2023-03-28 18:32:25 +0300

    some client code changes

--- Day8QuickSort/QuickSort/Program.cs
+++ Day8QuickSort/QuickSort/Program.cs
@@ -1,13 +1,18 @@
 ﻿// быстрая сортировка
 
-using static Sorting;
+// using static Sorting;
 
-int size = 10;
-var arr = size.CreateArray()
-            .Show()
-            .SortQuick(0, size - 1)
-            .Show()
-            ;
+// int size = 10;
+// var arr = size.CreateArray()
+//             .Show()
+//             .SortQuick(0, size - 1)
+//             .Show()
+//             ;
 
 
+int[] arr2 = Infrastructure.CreateArray(10, 0, 12);
 
+Infrastructure.Show(arr2);
+
+Sorting.SortQuick(arr2, 0, arr2.Length - 1);
+Infrastructure.Show(arr2);
