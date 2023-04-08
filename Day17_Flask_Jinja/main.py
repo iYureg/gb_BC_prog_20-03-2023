2e9ef14f9498dc883359634b1b1b48545e43f601
Yurii <iyureg@gmail.com>
2023-04-08 18:08:58 +0300

    Day17 flask_reg_log_final

--- Day17_Flask_Jinja/main.py
+++ Day17_Flask_Jinja/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from LoginForm import Lf
+from AuthForm import AuthF
 #from random import randint as rd
 
 app = Flask(__name__)
@@ -13,9 +14,32 @@ def main():
 def reg():
     form = Lf()
     if form.validate_on_submit():
-        pass
+        if form.password_again.data != form.password.data:
+            return render_template('register.html', title='Регистрация', form=form, 
+                                    message = 'Пароли не совпадают')
+
+        with open('file.txt', 'a', encoding='utf-8') as file:
+            file.write(f"{form.name.data};{form.email.data};{form.password.data}\n")
+        return render_template('register.html', message='Регистрация прошла успешно')        
 
     return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    form=AuthF()
+    if form.validate_on_submit():
+        with open('file.txt', 'r', encoding='utf-8') as file:
+            data =' '.join(file.readlines())
+
+        if form.email.data not in data:
+            return render_template('login.html', form=form, message='Вы не зарегистрированы')
+        else:
+            for i in data.split():
+                if form.email.data in i:
+                    if i.split(';')[-1] == form.password.data:
+                        return render_template('login.html', form=form, message='Вы успешно авторизовались')
+
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
