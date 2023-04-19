b8dabdb66cf24b12f999176584fdf816bddf7be7
Yurii <iyureg@gmail.com>
2023-04-19 23:37:16 +0300

    add day26 ORM

---
+++ Day26_ORM_SQLite3/Orm/App.cs
@@ -0,0 +1,17 @@
+public class App<T>
+    where T : Worker
+{
+    IRepository<T> current;
+
+    public App(IRepository<T> repo) => current = repo;
+
+    public void Create(T item) => current.Create(item);
+
+    public void Update(int id, T modified) =>
+        current.Update(id, modified);
+
+    public void Remove(int id) => current.Delete(id);
+
+    public void Print() =>
+        Console.WriteLine(String.Join<T>('\n', current.Read()));
+}
---
+++ Day26_ORM_SQLite3/Orm/Program.cs
@@ -0,0 +1,13 @@
+﻿App<Worker> app = new App<Worker>(new ListRepository());
+
+app.Create(new("Василий Иванов", 22, 3312));
+app.Create(new("Василий Петров", 44, 4562));
+app.Create(new("Иван Васильев", 33, 1245));
+
+app.Print(); Console.WriteLine();
+
+app.Remove(2);
+app.Print(); Console.WriteLine();
+
+app.Update(1, new("Sergey Kam", 122, 31312));
+app.Print(); Console.WriteLine();
